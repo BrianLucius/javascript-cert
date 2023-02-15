485682c067ef54e514aa7e0b32efbca907efbbba
Brian Lucius <brian@brianlucius.net>
2023-02-14 18:01:07 -0700

    JSCert - Module 1 and Module 2

---
+++ module_2/lab1.js
@@ -0,0 +1,23 @@
+// let rose_price = 8;
+// let rose_qty = 70;
+// let lily_price = 10;
+// let lily_qty = 50;
+// let tulip_price = 2;
+// let tulip_qty = 120;
+
+// console.log("Rose - unit price: " + rose_price + ", quantity: " + rose_qty + ", value: " + rose_price * rose_qty);
+// console.log("Lily - unit price: " + lily_price + ", quantity: " + lily_qty + ", value: " + lily_price * lily_qty);
+// console.log("Tulip - unit price: " + tulip_price + ", quantity: " + tulip_qty + ", value: " + tulip_price * tulip_qty);
+// console.log("Total: " + ((rose_price * rose_qty) + (lily_price * lily_qty) + (tulip_price * tulip_qty)));
+
+const rose_price = 8;
+let rose_qty = 50;
+const lily_price = 10;
+let lily_qty = 20;
+const tulip_price = 2;
+let tulip_qty = 120;
+
+console.log("Rose - unit price: " + rose_price + ", quantity: " + rose_qty + ", value: " + rose_price * rose_qty);
+console.log("Lily - unit price: " + lily_price + ", quantity: " + lily_qty + ", value: " + lily_price * lily_qty);
+console.log("Tulip - unit price: " + tulip_price + ", quantity: " + tulip_qty + ", value: " + tulip_price * tulip_qty);
+console.log("Total: " + ((rose_price * rose_qty) + (lily_price * lily_qty) + (tulip_price * tulip_qty)));
---
+++ module_2/variables.js
@@ -0,0 +1,17 @@
+let height = 180;
+let anotherHeight = height;
+let weight;
+console.log(height);
+console.log(anotherHeight);
+weight = 70
+console.log(weight);
+
+let steps = 100;
+console.log(steps);
+steps = 180
+console.log(steps);
+let greeting = "Hello!";
+console.log(greeting + steps)
+
+const greeting2 = "Hello again!"
+// greeting2 = "Goodbye"
